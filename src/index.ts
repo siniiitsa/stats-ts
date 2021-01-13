71f606e0ded28ddbae4ccfae1dbd07862a451634
Sergey <siniiitsa@gmail.com>
2021-01-13 11:51:30 +0200

    add MatchResults enum

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,20 @@ const matches = fs
   .split('\n')
   .map((row: string): string[] => row.split(','));
 
+enum MatchResults {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
+
 let manUnitedWinds = 0;
 for (let match of matches) {
-  if (match[1] === 'Man United' && match[5] === 'H') manUnitedWinds++;
-  if (match[2] === 'Man United' && match[5] === 'A') manUnitedWinds++;
+  if (match[1] === 'Man United' && match[5] === MatchResults.HomeWin) {
+    manUnitedWinds++;
+  }
+  if (match[2] === 'Man United' && match[5] === MatchResults.AwayWin) {
+    manUnitedWinds++;
+  }
 }
 
 console.log(`Man United has won ${manUnitedWinds} matches`);
