5ca13269f1fbe002d624840adb2d621511ea891c
Sergey <siniiitsa@gmail.com>
2021-01-13 11:08:27 +0200

    implement basic app run

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
 import fs from 'fs';
 
-fs.readFileSync('f');
+const matches = fs
+  .readFileSync('football.csv', { encoding: 'utf-8' })
+  .split('\n')
+  .map((row: string): string[] => row.split(','));
+
+let manUnitedWinds = 0;
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === 'H') manUnitedWinds++;
+  if (match[2] === 'Man United' && match[5] === 'A') manUnitedWinds++;
+}
+
+console.log(`Man United has won ${manUnitedWinds} matches`);
